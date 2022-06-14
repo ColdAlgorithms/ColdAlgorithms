87b9055a7d32bc6d19a59a8e308862df3e8bae61
ColdAlgorithms <88799028+ColdAlgorithms@users.noreply.github.com>
2022-06-14 22:22:00 +0300

    Update transform.py

--- transform.py
+++ transform.py
@@ -11,3 +11,22 @@ def transform(s1, s2):
     for i in range(len(l2)):
         int_l2.append(int(l2[i]))
     return list(map(lambda x,y : x*y, int_l1, int_l2))
+
+
+def transform2(s1, s2):
+    result = []
+    s1 = s1.split()
+    s2 = s2.split()
+    list1 = list(map(int, s1))
+    list2 = list(map(int, s2))
+    zipped = zip(list1, list2)
+    for i in list(zipped):
+        result.append(i[0] * i[1])
+
+    return(result)
+
+def main():
+    transform("1 5 3", "2 6 -1")
+
+if __name__ == "__main__":
+    main()
